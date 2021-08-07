1ff12ff66602139970119f07db0fee64b4cf9f77
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2021-08-07 15:14:28 +0700

    add devise for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_150018) do
+ActiveRecord::Schema.define(version: 2021_08_07_081248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "album_images", force: :cascade do |t|
-    t.belongs_to :album
     t.string "img_url"
   end
 
@@ -26,7 +25,6 @@ ActiveRecord::Schema.define(version: 2021_07_25_150018) do
     t.boolean "is_public"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.belongs_to :user
   end
 
   create_table "follower", force: :cascade do |t|
@@ -42,18 +40,23 @@ ActiveRecord::Schema.define(version: 2021_07_25_150018) do
     t.boolean "is_public"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.belongs_to :user
   end
 
   create_table "users", force: :cascade do |t|
     t.boolean "is_admin"
-    t.string "email"
     t.string "password"
     t.string "first_name"
     t.string "last_name"
     t.string "avatar_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
