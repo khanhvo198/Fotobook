287e82b204aa67a4fec09ec2b926d822f824969a
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2021-08-12 21:12:05 +0700

    fix edit avatar in profile

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -1,20 +1,20 @@
-$(function() {
+// $(function() {
 
-    $(".basic-info input[type='file']").on('change', function() {
-        if (this.files && this.files[0]) {
-            const reader = new FileReader();
+//     $(".basic-info input[type='file']").on('change', function() {
+//         if (this.files && this.files[0]) {
+//             const reader = new FileReader();
             
-            reader.onload = function (e) {
-                $('.basic-info #avatar').attr('src', e.target.result)
-                    .width(180)
-                    .height(180);
-            };     
-            reader.readAsDataURL(this.files[0]);    
-            // $(".basic-info .change-avatar").text("Save")  
-        }
-    })
+//             reader.onload = function (e) {
+//                 $('.basic-info #avatar').attr('src', e.target.result)
+//                     .width(180)
+//                     .height(180);
+//             };     
+//             reader.readAsDataURL(this.files[0]);    
+//             // $(".basic-info .change-avatar").text("Save")  
+//         }
+//     })
     
         
     
 
-})
+// })
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'photos/index'
   devise_for :users
   get 'home', to: 'home#index'
 
@@ -6,6 +7,14 @@ Rails.application.routes.draw do
   get 'edit_profile' , to: 'profiles#edit'
   put 'update_info', to: 'profiles#update_info'
   put 'update_password', to: 'profiles#update_password'
+
+
+
+
+
+
+  resource 'photos'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
