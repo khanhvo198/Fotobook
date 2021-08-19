fdca80a0f1ca58f9be10c1f89fc2e4a35fd461d4
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2021-08-19 23:59:22 +0700

    edit album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,6 @@
 class AlbumsController < ApplicationController
 
-
+    skip_before_action :verify_authenticity_token
 
     def create
         @album = Album.new(album_params)
@@ -17,10 +17,59 @@ class AlbumsController < ApplicationController
 
     end
 
+    def edit
+        @album = Album.find(params[:id])
+    end
+
+    def update
+        @album = Album.find(params[:id])
+
+
+        @remain_files = params[:remains_file]
+
+
+        if @remain_files.nil?
+            @album.remove_photos_url!
+        else
+            @remains = @album.photos_url
+            @remains_index = @remain_files.map{ |file| @album.photos_url_urls.index(file) }
+            @remains.each_with_index do |photo,index| 
+                unless @remains_index.include?(index) 
+                    @delete_at = @remains.delete_at(index)
+                    @delete_at.try(:remove!)
+                end
+            end
+            @album.photos_url = @remains
+
+        end
+
+
+        @photos_new = params[:photos_url]
+        unless @photos_new.nil?
+            @album.photos_url += @photos_new
+        end
+
+        @album.title = params[:title]
+        @album.description = params[:description]
+        @album.privacy = params[:privacy]
+
+        if @album.save
+            redirect_to profile_path(id: current_user.id)
+        else
+            render :edit
+        end
+    end
+
+
+    def destroy
+        
+    end
+
 
     def album_params
-        params.permit({ photos_url: []}, :title, :description)
+        params.permit({ photos_url: []}, :title, :description, :privacy)
     end
+
 
 
 end
--- app/javascript/packs/album.js
+++ app/javascript/packs/album.js
@@ -1,89 +1,85 @@
-$(function() {
+// $(function() {
 
-    console.log('hello')
-
-
-
-    let imageUploadInAlbum = []
+//     let imageUploadInAlbum = []
 
 
-    $('.new-album #photo_url').on('change', function() {
-        inputFiles = this
-        for(const file of inputFiles.files) {
-            imageUploadInAlbum.push(file)
-        }
-        previewPhoto(this)
+//     $('.new-album #photo_url').on('change', function() {
+//         inputFiles = this
+//         for(const file of inputFiles.files) {
+//             imageUploadInAlbum.push(file)
+//         }
+//         previewPhoto(this)
 
-    })
+//     })
 
-    $('.new-album').on('click', '#btn-delete-photo', function() {
-        // let dataFile = this.data('data-file')
-        console.log(this)
-        let indexRemove = imageUploadInAlbum.indexOf($(this).data('data-file'))
-        // console.log(imageUploadInAlbum)
-        // console.log(indexRemove)
-        imageUploadInAlbum.splice(indexRemove,1)
-        $(this).parent().parent().remove()
-    })
+//     $('.new-album').on('click', '#btn-delete-photo', function() {
+//         // let dataFile = this.data('data-file')
+//         console.log(this)
+//         let indexRemove = imageUploadInAlbum.indexOf($(this).data('data-file'))
+//         // console.log(imageUploadInAlbum)
+//         // console.log(indexRemove)
+//         imageUploadInAlbum.splice(indexRemove,1)
+//         $(this).parent().parent().remove()
+//     })
 
-    function previewPhoto(input) {
-        if (input.files && input.files.length) {
-            // $('.new-album .form-new-album .preview-photo-upload').removeClass('form-row')
-            for(const file of input.files) {
-                const reader = new FileReader();
-                reader.onload = function (e) {
-                        const divForImage = `
-                            <div class="col-md-2 mt-3">
-                                <div class="position-relative">
-                                    <a class='position-absolute' style="top: 0; right: 0; color: white; z-index:100; cursor: pointer" id="btn-delete-photo" data-file='${file}'><i class="fas fa-window-close"></i></a>
-                                    <img width='180' height='180' src='${e.target.result}' class='position-absolute' >
-                                </div>
-                            </div>
-                            `
-                        $('.new-album .form-new-album .preview-photo-upload').append(divForImage)
-                };
+//     function previewPhoto(input) {
+//         if (input.files && input.files.length) {
+//             // $('.new-album .form-new-album .preview-photo-upload').removeClass('form-row')
+//             for(const file of input.files) {
+//                 const reader = new FileReader();
+//                 reader.onload = function (e) {
+//                         const divForImage = `
+//                             <div class="col-md-2 mt-3">
+//                                 <div class="position-relative">
+//                                     <a class='position-absolute' style="top: 0; right: 0; color: white; z-index:100; cursor: pointer" id="btn-delete-photo" data-file='${file}'><i class="fas fa-window-close"></i></a>
+//                                     <img width='180' height='180' src='${e.target.result}' class='position-absolute' >
+//                                 </div>
+//                             </div>
+//                             `
+//                         $('.new-album .form-new-album .preview-photo-upload').append(divForImage)
+//                 };
     
-                reader.readAsDataURL(file);
-            }
-            $('.new-album .form-new-album .preview-photo-upload').height($('.preview-photo-upload').height())
-            input.value = null
-        }
-    }
+//                 reader.readAsDataURL(file);
+//             }
+//             $('.new-album .form-new-album .preview-photo-upload').height($('.preview-photo-upload').height())
+//             input.value = null
+//         }
+//     }
 
-    $('#submit-album').on('click', function(e) {
-        e.preventDefault()
-        let formData = new FormData()
-
-
-
-        imageUploadInAlbum.forEach(file => {
-            formData.append('photos_url[]', file)
-        })
-        let title = $('#title').val()
-        let privacy = $('#privacy').val()
-        let description = $('#description').val()
-        // console.log(privacy)
-        // console.log(description)
-        formData.append('title', title)
-        formData.append('privacy',privacy)
-        formData.append('description', description)
+//     $('#submit-album').on('click', function(e) {
+//         e.preventDefault()
+//         let formData = new FormData()
 
 
-        $.ajax({
-            url: '/albums',
-            method: 'post',
-            data: formData,
-            success: function(data) {
-                console.log(data)
-            },
-            cache: false,
-            processData: false,
-            contentType: false,
-        })
-    })
+
+//         imageUploadInAlbum.forEach(file => {
+//             formData.append('photos_url[]', file)
+//         })
+//         let title = $('#title').val()
+//         let privacy = $('#privacy').val()
+//         let description = $('#description').val()
+//         // console.log(privacy)
+//         // console.log(description)
+//         formData.append('title', title)
+//         formData.append('privacy',privacy)
+//         formData.append('description', description)
+
+
+//         $.ajax({
+//             url: '/albums',
+//             method: 'post',
+//             data: formData,
+//             success: function(data) {
+//                 console.log(data)
+//             },
+//             cache: false,
+//             processData: false,
+//             contentType: false,
+//         })
+//     })
     
 
 
 
 
-})
+// })
